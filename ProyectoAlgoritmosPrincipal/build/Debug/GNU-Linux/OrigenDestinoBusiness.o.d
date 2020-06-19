7cef27e70b47ff68c15e8ba565e03ecdd6e0d69c
Yarethleal <yarethleal65@gmail.com>
2020-06-18 19:45:14 -0600

    Data y Business para Origen-Destino del avion

---
+++ ProyectoAlgoritmosPrincipal/build/Debug/GNU-Linux/OrigenDestinoBusiness.o.d
@@ -0,0 +1,6 @@
+build/Debug/GNU-Linux/OrigenDestinoBusiness.o: OrigenDestinoBusiness.cpp \
+ OrigenDestinoBusiness.h OrigenDestinoDataSingleton.h
+
+OrigenDestinoBusiness.h:
+
+OrigenDestinoDataSingleton.h:
---
+++ ProyectoAlgoritmosPrincipal/build/Debug/GNU-Linux/OrigenDestinoDataSingleton.o.d
@@ -0,0 +1,4 @@
+build/Debug/GNU-Linux/OrigenDestinoDataSingleton.o: \
+ OrigenDestinoDataSingleton.cpp OrigenDestinoDataSingleton.h
+
+OrigenDestinoDataSingleton.h:
